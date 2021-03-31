d8d07fecaeb9ae4aff96be28107f1b456d01f36b
Yura65 <mrzaker16@gmail.com>
2021-04-01 02:52:55 +0400

    Initial commit

---
+++ hm1.py
@@ -0,0 +1,9 @@
+age = 18
+name = 'Max'
+print(age, name)
+
+str = input("Введите первую строку ")
+num = int(input("Введите первое число "))
+str1 = input("Введите вторую строку ")
+num2 = int(input("Введите второе число "))
+print(str, num, str1, num2)
---
+++ hm3.py
@@ -0,0 +1,6 @@
+n = int(input("Введите число n "))
+n_ = str(n)
+n2 = n_ + n_
+n3 = n2 + n_
+total = n + int(n2) + int(n3)
+print("Сумма чисел n + nn + nnn - %d" % total)
